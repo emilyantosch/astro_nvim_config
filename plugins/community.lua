b848d283adc2cc3ee58434a1b02c4b7a800c984e
ericantosch <ericantosch@web.de>
2023-04-08 17:55:16 +0200

    New Commit with merge

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,11 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-<<<<<<< HEAD
   { import = "astrocommunity.git.neogit" },
-=======
-
->>>>>>> b195f1423252e84772976e4a1e8a3231de352fdf
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.project.project-nvim" },
