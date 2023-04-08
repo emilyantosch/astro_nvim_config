b195f1423252e84772976e4a1e8a3231de352fdf
Emily Antosch <emilylucia.antosch@haw-hamburg.de>
2023-04-08 17:40:21 +0200

    First changes

--- mappings.lua
+++ mappings.lua
@@ -9,17 +9,22 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bv"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" },
+    ["<leader>Pf"] = { "<cmd>Telescope projects<cr>", desc = "Find Projects" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>P"] = { name = "Projects" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
